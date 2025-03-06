4d7653750d7414952ae5d029711eb4889d3dd4fa
Ranjith S <ranjith.sajeev@gmail.com>
2025-03-06 10:55:49 +0530

    Add favicons and codes to head

--- layout/head.php
+++ layout/head.php
@@ -4,5 +4,11 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo ("Wiki Conference India 2025 - Kochi, Kerala, India"); ?></title>
+    <link rel="icon" type="image/png" href="ico/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="ico/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="WCI2025" />
+    <link rel="manifest" href="ico/site.webmanifest" />
     <link rel="stylesheet" href="css/style.css">
   </head>
